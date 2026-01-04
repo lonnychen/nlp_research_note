c013bf09bc4f20206da148cf71ccf76e8537192b
lonnychen <lonny.cl.chen@gmail.com>
2026-01-04 22:01:51 +0100

    Fetch transcripts working for US/UK channels

--- data/data_collection.py
+++ data/data_collection.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import re
+import time
 import yt_dlp
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
+from youtube_transcript_api.proxies import WebshareProxyConfig
+from youtube_transcript_api._errors import TranscriptsDisabled
+
 
 def _get_shorts_video_ids(
     channel_search_url: str,
@@ -104,25 +108,57 @@ def get_channels_shorts_video_ids(channels: list[dict]) -> pd.DataFrame:
     return pd.concat(df_channels_list)
 
 
-def get_transcript_from_video_id(video_id: str, language_code: str) -> str:
+def get_transcript_from_video_id(
+    video_id: str,
+    language_code: str,
+    use_proxy: bool = True,
+    retries: int = 10
+) -> str:
     '''
 
-    Apply per-row to get a video's transcript in the specified language. Asserts `ValueError` if not available.
+    Apply per row to obtain a video's transcript in the specified language. Handles connection or unavailable transcript errors, so it should not crash due to an exception.
 
     Args:
         video_id (str): The YouTube video ID.
         language_code (str): The target language code.
+        use_proxy (bool): Use IP proxies to avoid IpBlocked from repeated transcript fetches.
+        retries (int): Number of retries for API requests.
 
     Returns:
         str: Returns the correct language transcript from YouTube ASR.
 
     '''
-    # Instantiate API methods
-    ytt_api = YouTubeTranscriptApi()
-    formatter = TextFormatter()
+    # Instantiate API method
+    if use_proxy:
+        # Workaround for IpBlocked: Webshare "Residential Proxy"
+        ytt_api = YouTubeTranscriptApi(
+            proxy_config=WebshareProxyConfig(
+                proxy_username='username', #FILL-IN LOCALLY
+                proxy_password='password', #FILL-IN LOCALLY
+                filter_ip_locations=['de']
+            )
+        )      
+    else:
+        ytt_api = YouTubeTranscriptApi()
+
+    # Get list of available languages
+    # Wrap around try/except in case of IncompleteRead, ProtocolError -> ChunkedEncodingError
+    for retry in range(retries):
+        try:
+            transcript_list = ytt_api.list(video_id)
+            break #success
+        except TranscriptsDisabled:
+            print(f"TranscriptsDisabled exception for video '{video_id}', skipping ...")
+            return 'TranscriptsDisabled'
+        except:
+            if retry == retries-1:
+                print(f"List retrieval failed for video '{video_id}', skipping ...")  
+                return 'ReachedListRetryLimit'
+            else:
+                print(f"List retrieval failed for video '{video_id}', retrying ...")    
+                time.sleep(10)
 
     # Check for expected language transcript
-    transcript_list = ytt_api.list(video_id)
     language_code_found = False
     for t in transcript_list:
         if t.language_code == language_code:
@@ -130,13 +166,27 @@ def get_transcript_from_video_id(video_id: str, language_code: str) -> str:
             break
     if not language_code_found:
         # Else NoTranscriptFound below
-        raise ValueError(f"Language code '{language_code}' not found for video '{video_id}'!")
+        print(f"Language code '{language_code}' not found for video '{video_id}', skipping ...")
+        return 'UnexpectedLanguage'
     
-    # Pipeline: find > fetch > convert to text
-    # By default this module always chooses manually created transcripts over automatically created ones
-    fetched_transcript = transcript_list.find_transcript([language_code]).fetch()
-    
-    # TODO: check for missing content
-    
+    # Pipeline: find > fetch
+    # By default, this module always chooses manually created transcripts over automatically created ones
+    # Wrap around try/except in case of IncompleteRead, ProtocolError -> ChunkedEncodingError
+    for retry in range(retries):
+        try:
+            fetched_transcript = transcript_list.find_transcript([language_code]).fetch()
+            print(f"Fetch completed for video '{video_id}'")
+            break #success
+        except:
+            if retry == retries-1:
+                print(f"Fetch failed for video '{video_id}', skipping ...")  
+                return 'ReachedFetchRetryLimit'
+            else:
+                print(f"Fetch failed for video '{video_id}', retrying ...")
+                time.sleep(10)
+                
+    # Pipeline: convert to text
+    formatter = TextFormatter()
     text_formatted_transcript = formatter.format_transcript(fetched_transcript)
+    
     return text_formatted_transcript
